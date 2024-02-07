0d933eec1c4ce7cd8542ed713a130cc9d7058a52
saikat.shill <saikat.shill@efficientsoftwaresolutions.com>
2024-02-07 16:32:54 +0600

    code refactoring

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from scrap import Scrapper
 
-scrapper = Scrapper(2)
-scrapper.read_table_data()
-scrapper.save_into_excel()
+
+class Main:
+    def __init__(self):
+        pass
+
+    def scrap_pdf(self, pdf_no):
+        scrapper = Scrapper(pdf_no)
+        scrapper.read_table_data()
+        scrapper.save_into_excel()
+
+
+Main().scrap_pdf(10)
